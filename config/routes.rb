2f3e12df50559e616b904e9946a170ea17f1a255
Jessica Kim <jessikim807@gmail.com>
2020-11-04 15:37:38 -0500

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,23 @@
 Rails.application.routes.draw do
   # Teacher Routes
-  get './teachers', to: 'teachers#index'
-  get './teachers/:id', to: 'teachers#show'
-  get './teachers/login', to: 'teachers#login'
-  get './teachers/stay_logged_in', to: 'teachers#stay_logged_in'
+  get '/teachers', to: 'teachers#index'
+  get '/teachers/:id', to: 'teachers#show'
+  post '/teachers/login', to: 'teachers#login'
+  get '/teachers/stay_logged_in', to: 'teachers#stay_logged_in'
 
   # Student Routes
-  get './students/', to: 'students#index'
-  get './students/:id', to: 'students#show'
-  get './students/login', to: 'students#login'
-  get './students/stay_logged_in', to: 'students#stay_logged_in'
+  get '/students', to: 'students#index'
+  get '/students/:id', to: 'students#show'
+  get '/students/stay_logged_in', to: 'students#stay_logged_in'
+  post '/students/login', to: 'students#login'
 
   # Assignment Routes
-  get './assignments', to: 'assignments#index'
-  get './assignments/:id', to: 'assignments#show'
-  post './assignments', to: 'assignments#create'
-  patch './assignments/:id', to: 'assignments#update'
-  delete './assignments/:id', to: 'assignments#destroy'
+  get '/assignments', to: 'assignments#index'
+  get '/assignments/:id', to: 'assignments#show'
+  post '/assignments', to: 'assignments#create'
+  patch '/assignments/:id', to: 'assignments#update'
+  delete '/assignments/:id', to: 'assignments#destroy'
 
   # TeacherKlasses Routes 
-  get './teacherklasses', to: 'teacher_klasses#index'
+  get '/teacherklasses', to: 'teacher_klasses#index'
 end
