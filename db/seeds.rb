18d082d10543cff1d98175d4fe52d2c967b6cf9e
Jessica Kim <jessikim807@gmail.com>
2020-11-04 10:26:33 -0500

    seeded database

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,9 +10,15 @@ Assignment.destroy_all
 
 puts 'creating students'
 Student.create(first_name: "Beth", last_name: Faker::Name.last_name, email: Faker::Internet.email, dob: Faker::Date.birthday(min_age: 10, max_age: 18), username: "Beth", password: "abc123")
+19.times do
+    Student.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, dob: Faker::Date.birthday(min_age: 10, max_age: 18), username: Faker::Name.first_name, password: "abc123")
+end
 
 puts 'creating teachers'
 Teacher.create(first_name: "Jessica", last_name: Faker::Name.last_name, subject: "Biology", email: Faker::Internet.email, username: "Jessica", password: "abc123")
+4.times do
+    Teacher.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, subject: Faker::Educator.subject, email: Faker::Internet.email, username: Faker::Name.first_name, password: "abc123")
+end
 
 puts 'creating assignments'
 Assignment.create(task: "Answer questions 1-10", course: "Biology", grade: "98", teacher_id: 1, student_id: 1)
