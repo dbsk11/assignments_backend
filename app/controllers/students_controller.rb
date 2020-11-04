80c97a6f91b5201d4052184835ddef6098b6b60d
Jessica Kim <jessikim807@gmail.com>
2020-11-04 15:22:27 -0500

    updated controllers

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,5 @@
 class StudentsController < ApplicationController
+  before_action :authorized, only: [:stay_logged_in]
 
   # GET /students
   def index
@@ -9,5 +10,34 @@ class StudentsController < ApplicationController
   # GET /students/1
   def show
     render json: @student
+  end
+
+  # Student Login
+  def login
+    @student = Student.find_by(username: params[:username])
+    if @student && @student.authenticate(params[:password])
+      wristband = encode_token({student_id: @student.id})
+      render json: {
+        student: StudentSerializer.new(@student),
+        token: wristband        
+      }
+    else
+      render json: {error: "Incorrect username and/or password"}
+    end 
+  end
+
+  def stay_logged_in
+    wristband = encode_token({student_id: @student.id})
+    render json: {
+      student: StudentSerializer.new(@student),
+      token: wristband
+    }
+  end
+
+  
+  private
+
+  def student_params
+      params.permit(:username, :password)
   end
 end
--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -11,4 +11,33 @@ class TeachersController < ApplicationController
     render json: @teacher
   end
 
+  # Teacher Login
+  def login
+    @teacher = Teacher.find_by(username: params[:username])
+    if @teacher && @teacher.authenticate(params[:password])
+        wristband = encode_token({teacher_id: @teacher.id})
+        render json: {
+            teacher: TeacherSerializer.new(@teacher),
+            token: wristband
+        }
+    else
+        render json: {error: "Incorrect username and/or password"}
+    end 
+  end
+
+  def stay_logged_in
+    wristband = encode_token({teacher_id: @teacher.id})
+    render json: {
+      teacher: TeacherSerializer.new(@teacher),
+      token: wristband
+    }
+  end
+
+  
+  private
+
+  def teacher_params
+    params.permit(:username, :password)
+  end
+
 end
