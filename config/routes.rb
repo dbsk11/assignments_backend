4ec05f6fed54400c281b6be0f8795c6d987e3a1f
Jessica Kim <jessikim807@gmail.com>
2020-11-04 15:35:32 -0500

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,23 @@
 Rails.application.routes.draw do
-  resources :student_klasses
-  resources :klasses
-  resources :teacher_klasses
-  resources :assignments
-  resources :students
-  resources :teachers 
+  # Teacher Routes
+  get './teachers', to: 'teachers#index'
+  get './teachers/:id', to: 'teachers#show'
+  get './teachers/login', to: 'teachers#login'
+  get './teachers/stay_logged_in', to: 'teachers#stay_logged_in'
+
+  # Student Routes
+  get './students/', to: 'students#index'
+  get './students/:id', to: 'students#show'
+  get './students/login', to: 'students#login'
+  get './students/stay_logged_in', to: 'students#stay_logged_in'
+
+  # Assignment Routes
+  get './assignments', to: 'assignments#index'
+  get './assignments/:id', to: 'assignments#show'
+  post './assignments', to: 'assignments#create'
+  patch './assignments/:id', to: 'assignments#update'
+  delete './assignments/:id', to: 'assignments#destroy'
+
+  # TeacherKlasses Routes 
+  get './teacherklasses', to: 'teacher_klasses#index'
 end
